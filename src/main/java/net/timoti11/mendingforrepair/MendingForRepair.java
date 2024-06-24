1237581ec50538150a4596a4e8ad39bcba8441c2
Timoti11 <timastanskov@mail.ru>
2024-06-24 12:57:04 +0400

    Initial commit

---
+++ src/main/java/net/timoti11/mendingforrepair/MendingForRepair.java
@@ -0,0 +1,17 @@
+package net.timoti11.mendingforrepair;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MendingForRepair extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
